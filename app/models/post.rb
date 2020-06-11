df390da49a83d08bcb6aef6d76b50bf72b0810b1
Nurislam Ab <you@example.com>
2020-06-11 22:24:29 +0500

    Add Comment model and made db migration for the comments table

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   validates :title, presence: true, uniqueness: true, length: { minimum: 12 }
 
   belongs_to :user
+  has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { in: 4..16 }
 
   has_many :posts
+  has_many :comments
 end
