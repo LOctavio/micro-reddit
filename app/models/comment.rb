713a2d74190303dfb08142777b1e5305c231edce
LOctavio <luis_ram1997@hotmail.com>
2020-06-11 12:59:50 -0500

    fix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-    validates :comment_body, presence: true
-    belongs_to :user
-    belongs_to :post
+  validates :comment_body, presence: true
+  belongs_to :user
+  belongs_to :post
 end
