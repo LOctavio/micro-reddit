f01be1a8edf3aa58b09f89187942ca360558c582
Nurislam Ab <you@example.com>
2020-06-11 22:00:10 +0500

    Made new migration for the user_id columng in Posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_155533) do
+ActiveRecord::Schema.define(version: 2020_06_11_165548) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "link"
-    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,5 +26,4 @@ ActiveRecord::Schema.define(version: 2020_06_11_155533) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "posts", "users"
 end
